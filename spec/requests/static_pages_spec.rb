1e8d6583a5961b35d427d81b34dbf79bea74dea6
Devon Veldhuis <devon.veldhuis@elementaltechnologies.com>
2014-10-06 10:50:32 -0700

    Added contacts page and parameterized base page title

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Home Page" do
 
     it "should have the content 'Sample App'" do
@@ -11,7 +13,7 @@ describe "StaticPages" do
 
     it "should have the right title" do
       visit "/static_pages/home"
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title("#{base_title} | Home")
     end
 
   end
@@ -23,13 +25,37 @@ describe "StaticPages" do
       expect(page).to have_content("Help")
     end
 
+    it "should have the right title" do
+      visit "/static_pages/help"
+      expect(page).to have_title("#{base_title} | Help")
+    end
+
   end
 
   describe "About Page" do
 
-    it "should have the content 'About Us'" do
+    it "should have the content 'About'" do
       visit "/static_pages/about"
-      expect(page).to have_content("About Us")
+      expect(page).to have_content("About")
+    end
+
+    it "should have the right title" do
+      visit "/static_pages/about"
+      expect(page).to have_title("#{base_title} | About Us")
+    end
+
+  end
+
+  describe "Contacts Page" do
+
+    it "should have the content 'Contacts'" do
+      visit "/static_pages/contacts"
+      expect(page).to have_content("Contacts")
+    end
+
+    it "should have the right title" do
+      visit "/static_pages/contacts"
+      expect(page).to have_title("#{base_title} | Contacts")
     end
 
   end
