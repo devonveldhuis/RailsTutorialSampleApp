67c8fb4c0f09b15836940a00aadd0d211ee8de5f
Devon Veldhuis <devon.veldhuis@elementaltechnologies.com>
2014-10-07 15:00:53 -0700

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do 
+  factory :user do
+    name      "Devon Veldhuis"
+    email     "devon@example.com"
+    password  "foobar"
+    password_confirmation "foobar"
+  end  
+end
