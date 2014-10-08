f9d4e730bcf5d699b6d0691e7494fd4179a8c3a4
Devon Veldhuis <devon.veldhuis@elementaltechnologies.com>
2014-10-08 12:52:38 -0700

    Finish user edit, update, index, and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
-FactoryGirl.define do 
+FactoryGirl.define do
   factory :user do
-    name      "Devon Veldhuis"
-    email     "devon@example.com"
-    password  "foobar"
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}
+    password "foobar"
     password_confirmation "foobar"
-  end  
+
+    factory :admin do
+      admin true
+    end
+  end
 end
